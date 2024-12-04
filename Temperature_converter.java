731987778e8e84e29588176d7bcc5942da25db59
Vaibhavi Dhenge <112369425+vaibhavidhenge23@users.noreply.github.com>
2024-12-04 11:46:16 +0530

    Add files via upload

---
+++ Temperature_converter.java
@@ -0,0 +1,65 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class Temperature_converter {
+// Declare the GUI Elements
+
+    public static JFrame frmMain;
+    public static JLabel lblCelsius;
+    public static JTextField textCelsius;
+    public static JLabel lblFahrenheit;
+    public static JTextField textFahrenheit;
+    public static JButton btnCalculateCtoF;
+    public static JButton btnCalculateFtoC;
+
+    public static void main(String[] args) {
+        // Set up the frame
+        frmMain = new JFrame("Temperature Converter");
+        frmMain.setSize(150, 200);
+        frmMain.setLayout(new FlowLayout());
+
+        // Create GUI Elements
+        lblCelsius = new JLabel("Celsius:");
+        textCelsius = new JTextField(10);
+        lblFahrenheit = new JLabel("Fahrenheit:");
+        textFahrenheit = new JTextField(10);
+        btnCalculateCtoF = new JButton("Convert C to F");
+        // Add ActionListener
+        btnCalculateCtoF.addActionListener(
+                new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Convert C to F
+                String cText = textCelsius.getText();
+                double c = Double.parseDouble(cText);
+                double f = (c * 9 / 5) + 32;
+                textFahrenheit.setText(String.valueOf(f));
+            }
+        }
+        );
+
+        btnCalculateFtoC = new JButton("Convert F to C");
+        // Add ActionListener
+        btnCalculateFtoC.addActionListener(
+                new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Convert F to C
+                String fText = textFahrenheit.getText();
+                double f = Double.parseDouble(fText);
+                double c = (f - 32) * 5 / 9;
+                textCelsius.setText(String.valueOf(c));
+            }
+        }
+        );
+
+        frmMain.add(lblCelsius);
+        frmMain.add(textCelsius);
+        frmMain.add(lblFahrenheit);
+        frmMain.add(textFahrenheit);
+        frmMain.add(btnCalculateCtoF);
+        frmMain.add(btnCalculateFtoC);
+
+        frmMain.setVisible(true);
+    }
+}
